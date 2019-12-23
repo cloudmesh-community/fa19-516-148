731df504a9e28bd6e58b70fd06241045dd114a51
Subramaniyam Raizada <sub@subraizada.com>
2019-12-22 21:18:48 -0500

    update pi configurator

--- project/cloudmesh-pi/cloudmesh/pi/command/pi.py
+++ project/cloudmesh-pi/cloudmesh/pi/command/pi.py
@@ -166,7 +166,7 @@ class PiCommand(PluginCommand):
 
     def setup_master(self, ips):
         commands = [
-            "'sudo kubeadm init'",
+            "'sudo kubeadm init --pod-network-cidr=10.244.0.0/16'",
             "'mkdir -p /home/pi/.kube'",
             "'sudo cp -i /etc/kubernetes/admin.conf /home/pi/.kube/config'",
             "'sudo chown 1000:1000 /home/pi/.kube/config'",
